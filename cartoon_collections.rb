09ef3b6cc38e6175c19fe646dd38c48fef829011
Joshua Collins <pc.refuge@gmail.com>
2020-08-03 03:59:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.map { |elm| "#{elm.capitalize}"}
+  array.map { |elm| "#{elm.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
